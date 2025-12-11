d281c9b4e356a36f9ce9e8a5d3e430bb7bfff2eb
Kaiky Moreira Yun <yunkaikyyun@gmail.com>
2025-12-11 20:11:35 -0300

    testes

--- projeto_orientacao_objetos/tests/unit/test_user_model.py
+++ projeto_orientacao_objetos/tests/unit/test_user_model.py
@@ -5,8 +5,6 @@ from app.models import User
 def test_password_hashing():
     password = "123456"
 
-    # Seu User não tem check_password; ele tem hash_password (conforme erro).
-    # Então validamos o hash comparando com check_password_hash.
     user = User(username="test", email="test@test.com", password_hash=generate_password_hash(password))
 
     assert check_password_hash(user.password_hash, password)
